ba578b186fc40f067989ee23601587c934485477
stephenlambertfs <stephenolambert@gmail.com>
2023-03-28 12:27:49 -0600

    added starter files

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-//fetch from the external api
-fetch("https://dog.ceo/api/breeds/list/all")
-.then((res) => res.json())
-.then(data => renderBreed(data))
-
-function renderBreed(breed){
-  const container = document.querySelector(".container")
-  console.log(breed.message)
-  const nav = document.querySelector(".breed-list")
-  nav.textContent = breed
-}
 
